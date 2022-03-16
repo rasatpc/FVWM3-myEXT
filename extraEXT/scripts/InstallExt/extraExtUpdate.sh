a0d816c43dfa72d5b6406bcca618c13858e163db
rasatpc <80579135+rasatpc@users.noreply.github.com>
2022-03-16 12:55:10 +0200

    Add files via upload

---
+++ extraEXT/scripts/InstallExt/extraExtUpdate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# By Jose Stack Overflow 9 Mar 2022
+# By rasat 10 March 2020
+
+# Tag all Read lines.
+sed -i 's/Read/# Read/g' ~/.fvwm/ExtraExt.sys
+sed -i 's/# # Read/# Read/g' ~/.fvwm/ExtraExt.sys
+
+# Reset line #CODE to TRUE/false
+sed -i "/#CODE/c\sed -i 's/TRUE/false/g' ExtInstaller.sh #CODE" ExtInstaller.sh
+
+# Update ExtraExt.sys extensions.
+search=$(awk 'BEGIN{OFS=ORS=""}{if(NR>1){print "|"}print $2;}' install.tmp)
+sed -i -r "s/^# (.*#($search))$/\1/g" ~/.fvwm/ExtraExt.sys
+
+# Update ExtInstaller.sh
+awk '
+NR==FNR {a[$2] = $1; next}
+$(NF-1) in a {$1 = a[$(NF-1)]}
+1' install.tmp ExtInstaller.sh > tmp.sh
+cp tmp.sh ExtInstaller.sh
+rm tmp.sh
+
+yad --width=350 --height=30 --title="Alert" --text-align=center \
+  --text="<b>   Left-click and Restart myExt.   </b>" --button=OK:0
